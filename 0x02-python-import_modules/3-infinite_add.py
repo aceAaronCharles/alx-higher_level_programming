bc550c73e519e487d4562d6c3bc12b98523e970d
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-01-07 04:09:04 -0800

    program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@
 if  __name__ == "__main__"
     """ Print the addition of all arguments."""
     import sys
-
     total = 0
-    for i in the range(len()sys.argv - 1):
-        total += int(sys.argv[i + 1])
+    if (len(sys.argv) > 1)
+        for i in range(1, len(sys.argv));
+        total += (int(sys.argv[i]))
     print("{}".format(total))
