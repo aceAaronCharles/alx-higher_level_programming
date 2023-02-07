2d7cf92f5225dab006ac183da0f206cfb2b16307
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-02-07 12:59:25 -0800

    input and output json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# This boy so acey
+
+
+
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# This boy so acey
+
+"""Defines a JSON file-reading function."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
