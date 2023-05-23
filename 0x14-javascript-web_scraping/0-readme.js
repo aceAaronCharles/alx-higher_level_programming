dc5811cf49ef49a69f634e3ef7e5c7d029e95507
Aaron0Chillz <charlesunlimited10@gmail.com>
2023-05-23 04:27:15 -0700

    script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+try {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const jsonData = JSON.parse(content);
+  console.log(jsonData);
+} catch (error) {
+  console.log(`Error occurred while reading the file: ${error}`);
+}
